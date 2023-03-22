99a03ca6d89bc24e1217ae80667ed26859c19da4
Kickcake <Kickvdstruik@gmail.com>
2023-03-22 11:57:54 +0100

    haha

--- site/database.php
+++ site/database.php
@@ -4,11 +4,11 @@ $database = "receptenboek";
 $username = "user";
 $password = "password";
 
-try {
+try { //and
   $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-} catch (PDOException $e) {
+} catch (PDOException $e) { //bitch
   echo "Connection failed: " . $e->getMessage();
 }
