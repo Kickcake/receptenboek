3d2fb08b95f5bd4fbbc4e9d281e9f36ee02d8994
Kickcake <Kickvdstruik@gmail.com>
2023-03-31 09:23:16 +0200

    k,

---
+++ site/compents/header.php
@@ -0,0 +1,4 @@
+<header>
+    <h1>Yo website </h1>
+
+</header>
---
+++ site/compents/nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/?sort=tijd"><button>tijd</button></a>
+    <a href="/?sort=ing"><button>ingredienten</button></a>
+    <a href="/?sort=niveau"><button>Niveau</button></a>
+</nav>
